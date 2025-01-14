8c6a7820f4321d55dfdb8f886ca17158ddeb2019
PandazNetwork <156095336+PandazNetwork@users.noreply.github.com>
2025-01-14 14:10:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from PandazNetwork'
 
-
 if __name__ == "__main__":
-    app.run()
+    # Use the PORT environment variable provided by Render
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 for local testing
+    app.run(host='0.0.0.0', port=port)  # Bind to 0.0.0.0 for Render compatibility
