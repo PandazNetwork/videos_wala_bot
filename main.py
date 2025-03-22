b391732da847289d60ba5c9eb95d7bd494823849
PandazNetwork <156095336+PandazNetwork@users.noreply.github.com>
2025-03-22 11:32:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from bot import Bot
+from flask import Flask, jsonify
+import threading
 
+# Initialize Flask app
+app = Flask(__name__)
+
+# Define the root endpoint
+@app.route("/")
+def home():
+    return jsonify({"message": "Welcome To PandazNetwork"})
+
+# Function to run the Flask app
+def run_flask():
+    app.run(host="0.0.0.0", port=8080)
 Bot().run()
